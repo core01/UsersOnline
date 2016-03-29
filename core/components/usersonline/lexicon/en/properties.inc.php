751ae7d4c8caa00b9c3a59196b21fca756a7a339
Roman Sadoyan <rs@rosa.pro>
2016-03-29 16:21:30 +0300

    lexicon fixed

--- core/components/usersonline/lexicon/en/properties.inc.php
+++ core/components/usersonline/lexicon/en/properties.inc.php
@@ -1,2 +1,3 @@
 <?php
-    $lang['useronline_prop_contexts'] = 'List of contexts for searching';
+    $_lang['usersonline_prop_contexts'] = 'List of contexts for searching';
+    $_lang['usersonline_prop_timeInterval'] = 'Time interval, time in seconds, during which the user is considered to be online';
--- core/components/usersonline/lexicon/ru/properties.inc.php
+++ core/components/usersonline/lexicon/ru/properties.inc.php
@@ -1,2 +1,3 @@
 <?php
-    $lang['useronline_prop_contexts'] = 'Список контекстов для поиска';
+    $_lang['usersonline_prop_contexts'] = 'Список контекстов для поиска';
+    $_lang['usersonline_prop_timeInterval'] = 'Временной интвервал в течении которого пользователь считается онлайн';
